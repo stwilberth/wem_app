91dc6146553b742f25b55d6922dbf1e7b00ab26c
Wilberth Loría <stwilberth@gmail.com>
2021-08-04 07:28:17 -0600

    rutas miselaneas

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => ['role:psicologo|admin']], function () {
         Route::get('/grupo/{id}',           App\Http\Livewire\Grupos\Show::class)               ->name('grupo');
         
         Route::get('/usuario/{id}',         App\Http\Livewire\Users\Show::class)                ->name('user');
-        Route::get('/usuarios',             App\Http\Livewire\Users\index::class)               ->name('users');
+        Route::get('/usuarios',             App\Http\Livewire\Users\Index::class)               ->name('users');
         Route::get('/hombres',              App\Http\Livewire\Hombre\Index::class)              ->name('hombres');
     });
     Route::get('/dashboard',            App\Http\Livewire\Dashboard\Index::class)           ->name('dashboard');    
@@ -40,3 +40,33 @@ Route::get('/hombre-login',         App\Http\Livewire\Hombre\Login::class)      
 
 Route::get('/',                     App\Http\Livewire\Welcome::class)                   ->name('welcome');
 Auth::routes();
+
+
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return 'Config cache cleared';
+}); 
+
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return 'View cache cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('optimize:clear');
+    return 'optimize:clear';
+});
